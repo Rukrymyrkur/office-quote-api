75176b2ab445a2a4c9b3177baa1ddaddfd72ef0d
Rukrymyrkur <surmilaviciute@gmail.com>
2020-11-07 22:29:14 +0200

    added cors

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const quotes = require('./Quotes');
 
 const app = express();
 
+app.use((req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET');
+    return res.status(200).json({});
+}
+});
+
 
 // homepage route
 app.get('/', (req, res) => res.render('index', {
