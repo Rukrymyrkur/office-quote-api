b5d2b6af5059316beb665b04498a3b23e8773d9a
Rukrymyrkur <surmilaviciute@gmail.com>
2020-11-08 00:25:24 +0200

    added cors again

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ const app = express();
 app.use((req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'GET');
-    return res.status(200).json({});
-}
 });
 
 
