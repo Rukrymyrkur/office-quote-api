c7a5b41901a1b8a92b25423e29910c717e74688b
Rukrymyrkur <surmilaviciute@gmail.com>
2020-11-09 13:38:07 +0200

    added more quotes

--- Quotes.js
+++ Quotes.js
@@ -40,6 +40,54 @@ const quotes = [
     "by": "Michael Scott",
     "from": "Season 5, 'Stress Relief",
     "sentence": "No, Rose, they are not breathing. And they have no arms or legs … Where are they? You know what? If we come across somebody with no arms or legs, do we bother resuscitating them? I mean, what quality of life do we have there?"
+    },
+    {
+    "id": 8,
+    "by": "Dwight Schrute",
+    "from": "Season 3, Product Recall",
+    "sentence": "Identity theft is not a joke, Jim! Millions of families suffer every year."
+    },
+    {
+    "id": 9,
+    "by": "Dwight Schrute",
+    "from": "Season 5, Stress Relief",
+    "sentence": "Today, smoking is going to save lives."
+    },
+    {
+    "id": 10,
+    "by": "Michael Scott",
+    "from": "Season 4, Money",
+    "sentence": "I am running away from my responsibilities. And it feels good."
+    },
+    {
+    "id": 11,
+    "by": "Kevin Malone",
+    "from": "Season 3, Beach Games",
+    "sentence": "I just want to lie on the beach and eat hot dogs. That’s all I’ve ever wanted."
+    },
+    {
+    "id": 12,
+    "by": "Angela Martin",
+    "from": "Season 3, Women’s Appreciation",
+    "sentence": "Sometimes the clothes at Gap Kids are too flashy, so I’m forced to go to the American Girl store and order clothes for large colonial dolls."
+    },
+    {
+    "id": 13,
+    "by": "Dwight Schrute",
+    "from": "Season 2, Office Olympics",
+    "sentence": "If I were buying my coffin, I would get one with thicker walls so you couldn’t hear the other dead people."
+    },
+    {
+    "id": 14,
+    "by": "Michael Scott",
+    "from": "Season 5, Stress Relief",
+    "sentence": "And I knew exactly what to do. But in a much more real sense, I had no idea what to do."
+    },
+    {
+    "id": 15,
+    "by": "Pam Beesly",
+    "from": "Season 9, Finale",
+    "sentence": "There’s a lot of beauty in ordinary things. Isn’t that kind of the point?"
     }
 ];
 
